a62c09f884d47d98e09c26878a12cd2197083791
Daniel Board <daveee@hotmail.com>
2023-09-03 12:09:46 +0100

    fixes

--- src/pages/partners/Index.jsx
+++ src/pages/partners/Index.jsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import Box from "./Box";
 import Header from "./Header";
 import Note from "./Note";
 
 const Partners = () => {
+  
+  useEffect(() => {
+    document.body.classList.add('smooth');
+  })
+
   return (
     <>
       <Header />
